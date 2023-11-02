c2e52e755049e79b4a204fb5be519f4bd9a24ac3
salihnsventures <muhammed.salih@nsventures.se>
2023-11-02 22:09:36 +0400

    login screen design

--- lib/screens/onboding/onboding_screen.dart
+++ lib/screens/onboding/onboding_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -10,6 +13,39 @@ class OnboardingScreen extends StatefulWidget {
 class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            width: MediaQuery.of(context).size.width * 1.7,
+            bottom: 200,
+            left: 100,
+            child: Image.asset('assets/Backgrounds/Spline.png'),),
+          Positioned.fill(child: BackdropFilter(filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10))),
+          const RiveAnimation.asset('assets/RiveAssets/shapes.riv'),
+          Positioned.fill(child: BackdropFilter(filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30), child: const SizedBox(),),),
+          const SafeArea(child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32.0),
+            child: Column(children: [
+              SizedBox(
+                width: 260,
+                child: Column(
+                  children: [
+                    Text('Learn design & code',
+                    style: TextStyle(
+                      fontSize: 60,
+                      fontFamily: 'Poppins',
+                      height: 1.2,
+                    ),
+                    ),
+                    Text('Don\'t skip design. Learn design and code, by building real app with Flutter and Swift. Complete courses about the best tools.')
+                  ],
+                ),
+              )
+            ],),
+          ))
+        ],
+      ),
+    );
   }
 }
