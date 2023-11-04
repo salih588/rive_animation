9c5b0c63bb6d3e3cb36ae739b86d7e871ca91b61
salihnsventures <muhammed.salih@nsventures.se>
2023-11-04 11:04:01 +0400

    working on start button animation

--- lib/screens/onboding/onboding_screen.dart
+++ lib/screens/onboding/onboding_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:rive/rive.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -11,6 +13,14 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+  late RiveAnimationController _btnAnimationController;
+
+  @override
+  void initState() {
+    _btnAnimationController = OneShotAnimation('active');
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,29 +30,66 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             width: MediaQuery.of(context).size.width * 1.7,
             bottom: 200,
             left: 100,
-            child: Image.asset('assets/Backgrounds/Spline.png'),),
-          Positioned.fill(child: BackdropFilter(filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10))),
+            child: Image.asset('assets/Backgrounds/Spline.png'),
+          ),
+          Positioned.fill(
+              child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10))),
           const RiveAnimation.asset('assets/RiveAssets/shapes.riv'),
-          Positioned.fill(child: BackdropFilter(filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30), child: const SizedBox(),),),
-          const SafeArea(child: Padding(
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+              child: const SizedBox(),
+            ),
+          ),
+          const SafeArea(
+              child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 32.0),
-            child: Column(children: [
-              SizedBox(
-                width: 260,
-                child: Column(
-                  children: [
-                    Text('Learn design & code',
-                    style: TextStyle(
-                      fontSize: 60,
-                      fontFamily: 'Poppins',
-                      height: 1.2,
-                    ),
-                    ),
-                    Text('Don\'t skip design. Learn design and code, by building real app with Flutter and Swift. Complete courses about the best tools.')
-                  ],
+            child: Column(
+              children: [
+                SizedBox(
+                  width: 260,
+                  child: Column(
+                    children: [
+                      Text(
+                        'Learn design & code',
+                        style: TextStyle(
+                          fontSize: 60,
+                          fontFamily: 'Poppins',
+                          height: 1.2,
+                        ),
+                      ),
+                      Gap(16),
+                      Text(
+                          'Don\'t skip design. Learn design and code, by building real app with Flutter and Swift. Complete courses about the best tools.')
+                    ],
+                  ),
                 ),
-              )
-            ],),
+                SizedBox(
+                  height: 64,
+                  width: 260,
+                  child: Stack(
+                    children: [
+                      RiveAnimation.asset('assets/RiveAssets/button.riv'),
+                      Positioned.fill(
+                        top: 8,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(CupertinoIcons.arrow_right),
+                            Gap(8),
+                            Text(
+                              'Start the course',
+                              style: TextStyle(fontWeight: FontWeight.w600),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ))
         ],
       ),
