a03a527456c2bd7dc50da6dd84f511f8800f377f
salihnsventures <muhammed.salih@nsventures.se>
2024-02-20 15:16:00 +0530

    working on sidenav

--- lib/components/side_menu.dart
+++ lib/components/side_menu.dart
@@ -1,5 +1,7 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+import 'package:rive_animation/components/info_card.dart';
 
 class SideMenu extends StatefulWidget {
   const SideMenu({Key? key}) : super(key: key);
@@ -12,7 +14,19 @@ class _SideMenuState extends State<SideMenu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(width: 288, height: double.infinity, color: const Color(0xFF17203A), child: Column(children: [],),),
+      body: Container(
+        width: 288,
+        height: double.infinity,
+        color: const Color(0xFF17203A),
+        child: SafeArea(
+          child: Column(
+            children: [
+              InfoCard(name: "Abu Anwar", profession: "Youtuber",),
+              ListTile(leading: SizedBox(height: 34, width: 34, child: RiveAnimation.asset("assets/RiveAssets/icons.riv"),),)
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
